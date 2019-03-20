0f2272397effa353e85224f98429371eaa650b88
block-cat <kfx2007@outlook.com>
2019-03-20 14:52:53 +0800

    robot commited.

--- work.py
+++ work.py
@@ -44,22 +44,43 @@ class Work(object):
             # 切换工作目录
             os.chdir(item.directory)
             print("正在检查{}".format(item.directory))
-            # 检查是否有未push的代码
-            status_str = subprocess.check_output(
-                ["git", "status"]).decode("utf-8")
+
             # 获取当前分支
             branch = cmd_executor(
                 "git rev-parse --abbrev-ref HEAD")
-            print(status_str)
+            # 检查是否有未提交的代码
+            status_str = subprocess.check_output(
+                ["git", "status"]).decode("utf-8")
+            # 检查当前工作目录是否有遗漏工作
             if "Changes not staged for commit" in status_str:
                 print("当前{}分支有尚未提交的代码，正在提交...".format(branch))
                 # 有尚未提交的代码
                 os.system("git add .")
                 os.system("git commit -m 'robot commited.'")
-            if "Your branch is ahead of" in status_str:
-                print("当前{}分支有未推送的代码，正在远端{}推送..".format(
-                    branch, item.origin))
-                # 把代码提交到远程桌面库
-                os.system("git push {} {}".format(item.origin, branch))
+
+            # 获取当前所有的分支
+            branches = [b.strip() for b in cmd_executor(
+                "git branch").replace("*", "").split("\n")]
+
+            # 切换分支
+            for branch in branches:
+                print('切换到{}分支'.format(branch))
+                os.system("git checkout {}".format(branch))
+                try:
+                    # 检查当前分支是否有未push的commit
+                    unpushed = cmd_executor(
+                        "git log {}/{}..{}".format(item.origin, branch, branch))
+                    if unpushed:
+                        print("当前{}分支有未推送的代码，正在远端{}推送..".format(
+                            branch, item.origin))
+                        # 把代码提交到远程桌面库
+                        os.system("git push {} {}".format(item.origin, branch))
+                except Exception as ex:
+                    # 远端并未创建分支，直接推送
+                    print("远端没有{}分支，正在远端{}推送..".format(
+                        branch, item.origin))
+                    # 把代码提交到远程桌面库
+                    os.system("git push {} {}".format(item.origin, branch))
 
             print("目录{}，检查完成".format(item.directory))
+            print('=========>(*^_^*)<==========')
